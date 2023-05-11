99f8d1a933a2ce378d9831638a4260bb7e72cd7b
Iuri Cristofaro <iuricristofaro@gmail.com>
2023-05-10 21:20:57 -0300

    falta no router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,43 +1,16 @@
 import './bootstrap';
-// import { createApp } from 'vue';
+
+
+import { createApp } from 'vue'
+
+import App from './components/App.vue'
+import router from './routes/routers.js'
 
 
 
-// import router from './routes/routers';
-
-
-// const app = createApp({});
-
-// import TestComponent from './components/TestComponent';
-// app.component('test-component', TestComponent);
-
-
-// app.mount('#app');
-// app.use(router);
-
-
-import Vue from'vue';
-window.Vue = require('vue');
- 
-import App from '../components/App.vue';
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './routes';
- 
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
- 
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
- 
-const app = new Vue({
-    el: '#app',
-    router: router,
-    render: h => h(App),
-});
+createApp(App)
+    .use(router)
+    .mount('#app')
 
 
 
